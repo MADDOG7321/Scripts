e51198fb09ae3bb1ce99b678b13f9099d15e3280
MADDOG7321 <33217862+MADDOG7321@users.noreply.github.com>
2019-09-12 13:26:10 +1000

    Add files via upload

---
+++ client.py
@@ -0,0 +1,27 @@
+from socket import *
+
+HOST = "127.0.0.1"
+PORT = 1024
+ADDRESS = (HOST, PORT)
+
+server = socket(AF_INET, SOCK_STREAM)
+server.connect(ADDRESS)
+message = bytes(server.recv(4096)).decode("utf8")
+data = message.split()
+print(message)
+
+filerequest = input("file to request: ")
+while filerequest not in data[9:]:
+    filerequest = input("file to request: ")
+
+server.send(bytes(filerequest, "utf8"))
+
+file = open(f"{filerequest}", 'wb')
+while True:
+    data = server.recv(32)
+    if data:
+        file.write(data)
+    else:
+        break
+
+file.close()
---
+++ server.py
@@ -0,0 +1,49 @@
+from socket import *
+from time import ctime
+import os
+import sys
+
+class ClientHandler():
+    def __init__(self, client, address):
+        self.client = client
+        self.address = address
+
+    def run(self):
+        print(f"sending {address} data...")
+
+        options = os.listdir("files")
+        data = ctime() + "\n What file you want?\n"
+        for file in options:
+            data += f"{file}\n"
+            
+        client.send(bytes(data, "utf8"))
+        print("finished [1/2].")
+
+        reply = client.recv(64).decode("utf8")
+        print(f"sending file {reply}")
+        file = open(f"files/{reply}", 'rb')
+        filedata = b'' + file.read()
+        print(sys.getsizeof(filedata))
+        client.send(filedata)
+        print("finished [2/2].")
+
+        client.close()
+
+HOST = "127.0.0.1"
+PORT = 1024
+ADDRESS = (HOST, PORT)
+
+server = socket(AF_INET, SOCK_STREAM)
+server.bind(ADDRESS)
+server.listen(5)
+
+while True:
+    try:
+        print("Waiting for connection...")
+        client, address = server.accept()
+        print(f"Connection from: {address}")
+        
+        handler = ClientHandler(client, address)
+        handler.run()
+    except KeyboardInterrupt:
+        break
